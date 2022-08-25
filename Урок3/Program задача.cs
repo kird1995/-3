4d6d67686040f2b8a6868a4870f0fce6b1c42f28
Владимир Александрович Кирдяшкин <kva@vast.su>
2022-08-25 18:03:11 +0300

    Добавьте файлы проекта.

---
+++ Урок3/Program задача.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Урок3
+{
+    internal class Program
+    {
+        static void Main(string[] args) { 
+        {
+            double x;
+            double k; 
+            x = GetNum("x"); 
+            k = GetNum("k");
+            GetAnswer(x, k); 
+            Console.ReadKey();
+        }
+        static void GetAnswer(double x, double k)
+        {
+            if (x <= -5)    
+            { 
+                Console.WriteLine($"Результат вычисления y = {2 * x + k}"); 
+                return;
+            }
+            if (x > -5 && x < 0)
+            {
+                Console.WriteLine($"Результат вычисления y = {x + 2 / k}");
+                return;
+            }
+            if (x == 0)
+            {
+                Console.WriteLine($"Результат вычисления y = {k * k}");
+                return;
+            }
+            if (x > 0 && x < 7)
+            {
+                Console.WriteLine($"Результат вычисления y = {k - x}");
+                return;
+            }
+            if (x >= 7)
+            {
+                Console.WriteLine($"Результат вычисления y = {k * k - Math.Pow(x, k)}");
+                return;
+            }
+
+            Console.WriteLine();
+        }
+
+
+        static double GetNum(string str)
+        { 
+            while (true)
+            {
+                Console.Write("Введите число {0}: ", str);
+                if (double.TryParse(Console.ReadLine(), out double number))
+                { 
+                    return number;
+                    break;
+                }
+
+                Console.WriteLine("Внимание, введено не число!");
+            }
+        }
---
+++ Урок3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Урок3
+{
+    internal class Program
+    {
+        static void Main(string[] args) { 
+        {
+            double x;
+            double k; // Пер. с заданм типом
+            x = GetNum("x"); // Метод задаем и пере.
+            k = GetNum("k");
+            GetAnswer(x, k); // имя метода (пер. которые он будет исполь).
+            Console.ReadKey();
+        }
+        static void GetAnswer(double x, double k)
+        {
+            if (x <= -5)
+            { // условие
+                Console.WriteLine($"Результат вычисления y = {2 * x + k}"); // выполнение условия 
+                return;
+            }
+            if (x > -5 && x < 0)
+            {
+                Console.WriteLine($"Результат вычисления y = {x + 2 / k}");
+                return;
+            }
+            if (x == 0)
+            {
+                Console.WriteLine($"Результат вычисления y = {k * k}");
+                return;
+            }
+            if (x > 0 && x < 7)
+            {
+                Console.WriteLine($"Результат вычисления y = {k - x}");
+                return;
+            }
+            if (x >= 7)
+            {
+                Console.WriteLine($"Результат вычисления y = {k * k - Math.Pow(x, k)}");
+                return;
+            }
+
+            Console.WriteLine();
+        }
+
+
+        static double GetNum(string str)
+        { // в консоли ввод чисел 
+            while (true)
+            {
+                Console.Write("Введите число {0}: ", str);
+                if (double.TryParse(Console.ReadLine(), out double number))
+                { // перевод значений в Double, //Console.ReadLine(), out double number условие ввода числовых значений.
+                    return number; // 
+                    break;
+                }
+
+                Console.WriteLine("Внимание, введено не число!");
+            }
+        }
